66431c34ff99d24f5f5938699ec53b47e6e96190
Vasilieva-Elena <elenademidova8487@gmail.com>
2026-04-06 14:20:27 +0300

    Update Entry.h

--- include/Entry.h
+++ include/Entry.h
@@ -1,1 +1,26 @@
+#pragma once
 
+#include <memory>
+#include <string>
+#include <vector>
+
+class Directory; // forward declaration
+
+class Entry : public std::enable_shared_from_this<Entry> {
+public:
+    std::string name;
+    bool hidden;
+
+    explicit Entry(const std::string& n);
+    virtual ~Entry() = default;
+
+    virtual std::vector<std::shared_ptr<Entry>> listAll() const;
+    virtual std::string displayName() const = 0;
+    virtual std::shared_ptr<Directory> asDirectory();
+    virtual void edit() = 0;
+    virtual void hide() = 0;
+    virtual void restore() = 0;
+    virtual bool isHidden() const;
+    virtual std::string getContent() const;
+    virtual void setContent(const std::string& content);
+};
