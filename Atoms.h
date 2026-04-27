4e437d1a522bcab7a0ff39306c181655823d8eb8
Vasilieva-Elena <elenademidova8487@gmail.com>
2026-04-27 20:24:31 +0300

    Update Atoms.h

--- Atoms.h
+++ Atoms.h
@@ -2,26 +2,22 @@
 #define ATOMS_H
 
 #include <string>
+#include <memory>
 
-// Предварительные объявления таблиц (чтобы избежать циклической зависимости)
 class StringTable;
 class SymbolTable;
 
-// Базовый класс для всех операндов
 class Operand {
 public:
     virtual ~Operand() = default;
     virtual std::string toString() const = 0;
 };
 
-// Операнды, которые могут использоваться в вычислениях (RValue)
 class RValue : public Operand {
 public:
     virtual ~RValue() = default;
-    // Не добавляет новых методов, только метка-интерфейс
 };
 
-// Операнд-число (константа)
 class NumberOperand : public RValue {
     int _value;
 public:
@@ -29,25 +25,28 @@ public:
     std::string toString() const override;
 };
 
-// Операнд-переменная (ссылка на таблицу символов)
 class MemoryOperand : public RValue {
     int _index;
     const SymbolTable* _symbolTable;
 public:
     MemoryOperand(int index, const SymbolTable* symbolTable);
     std::string toString() const override;
+    
+    // Добавленный оператор сравнения
+    bool operator==(const MemoryOperand& other) const;
 };
 
-// Операнд-строка (ссылка на таблицу строк)
 class StringOperand : public Operand {
     int _index;
     const StringTable* _stringTable;
 public:
     StringOperand(int index, const StringTable* stringTable);
     std::string toString() const override;
+    
+    // Добавленный оператор сравнения
+    bool operator==(const StringOperand& other) const;
 };
 
-// Операнд-метка (для переходов, в этом задании не используется, но по иерархии нужен)
 class LabelOperand : public Operand {
     int _labelID;
 public:
