5025cd1405f77b69ae74dd761dc3329c3eeec72e
Vasilieva-Elena <elenademidova8487@gmail.com>
2026-04-28 20:55:46 +0300

    Update SymbolTable.cpp

--- SymbolTable.cpp
+++ SymbolTable.cpp
@@ -1,5 +1,5 @@
 #include "SymbolTable.h"
-#include "Atoms.h"        // теперь нужно для MemoryOperand
+#include "Atoms.h"
 #include <algorithm>
 
 const SymbolTable::TableRecord& SymbolTable::operator[](int index) const {
@@ -7,23 +7,19 @@ const SymbolTable::TableRecord& SymbolTable::operator[](int index) const {
 }
 
 std::shared_ptr<MemoryOperand> SymbolTable::add(const std::string& name) {
-    // Ищем запись по имени
     auto it = std::find_if(_records.begin(), _records.end(),
         [&name](const TableRecord& rec) { return rec._name == name; });
     if (it != _records.end()) {
         int index = static_cast<int>(it - _records.begin());
-        // Возвращаем уже созданный операнд (если он есть)
         if (index < static_cast<int>(_operands.size()) && _operands[index]) {
             return _operands[index];
         }
-        // На всякий случай создаём, если вдруг не было
         auto operand = std::make_shared<MemoryOperand>(index, this);
         if (index >= static_cast<int>(_operands.size()))
             _operands.resize(index + 1);
         _operands[index] = operand;
         return operand;
     }
-    // Новая запись
     int newIndex = static_cast<int>(_records.size());
     _records.push_back(TableRecord(name));
     auto operand = std::make_shared<MemoryOperand>(newIndex, this);
@@ -36,4 +32,10 @@ std::ostream& operator<<(std::ostream& os, const SymbolTable& st) {
         os << i << " " << st._records[i]._name << std::endl;
     }
     return os;
+}
+
+std::shared_ptr<MemoryOperand> SymbolTable::alloc() {
+    static int counter = 0;
+    std::string tempName = "__tmp" + std::to_string(++counter);
+    return add(tempName);
 }
